2ddce297f049e1bfb4adef872d92b95c4cdbb480
Tsung-Ta Wu <edison1998402@gmail.com>
2020-08-14 13:41:04 +0800

    Update Hungarian.cpp

--- Graph/Matching/Hungarian.cpp
+++ Graph/Matching/Hungarian.cpp
@@ -1,21 +1,18 @@
 // Maximum Cardinality Bipartite Matching
 // Worst case O(nm)
 
-struct Graph{ 
-    static const int MAXN = 5003;
-    vector<int> G[MAXN];
-    int n, match[MAXN], vis[MAXN];
+struct Graph {
+    vector<vector<int>> G;
+    vector<int> match, vis;
 
-    void init(int _n){
-        n = _n;
-        for (int i=0; i<n; i++) G[i].clear();
+    void init(int n) {
+        G = vector<vector<int>>(n);
     }
-
-    bool dfs(int u){
-        for (int v:G[u]){
+    bool dfs(int u) {
+        for (int v : G[u]) {
             if (vis[v]) continue;
-            vis[v]=true;
-            if (match[v]==-1 || dfs(match[v])){
+            vis[v] = true;
+            if (match[v] == -1 || dfs(match[v])) {
                 match[v] = u;
                 match[u] = v;
                 return true;
@@ -23,14 +20,13 @@ struct Graph{
         }
         return false;
     }
-
-    int solve(){
+    int solve() {
         int res = 0;
-        memset(match,-1,sizeof(match));
-        for (int i=0; i<n; i++){
-            if (match[i]==-1){
-                memset(vis,0,sizeof(vis));
-                if ( dfs(i) ) res++;
+        fill(match.begin(), match.end(), -1);
+        for (int i = 0; i < (int)G.size(); i++) {
+            if (match[i] == -1) {
+                fill(vis.begin(), vis.end(), 0);
+                if (dfs(i)) res++;
             }
         }
         return res;

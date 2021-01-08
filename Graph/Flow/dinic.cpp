9096940aa17a674ffb5cc2c98eac7d1cecd8fa56
Tsung-Ta Wu <edison1998402@gmail.com>
2021-01-08 17:30:44 +0800

    formatting

--- Graph/Flow/dinic.cpp
+++ Graph/Flow/dinic.cpp
@@ -32,9 +32,9 @@
 -------------------------------------------------------
 
 const long long INF = 1LL<<60;
-struct Dinic {  //O(VVE), with minimum cut 
+struct Dinic {  // O(VVE), with minimum cut
     static const int MAXN = 5003;
-    struct Edge{
+    struct Edge {
         int u, v;
         long long cap, rest;
     };
@@ -43,9 +43,9 @@ struct Dinic {  //O(VVE), with minimum cut
     vector<Edge> edges;
     vector<int> G[MAXN];
 
-    void init(){
+    void init() {
         edges.clear();
-        for ( int i = 0 ; i < n ; i++ ) G[i].clear();
+        for (int i = 0; i < n; i++) G[i].clear();
         n = 0;
     }
 
@@ -53,33 +53,35 @@ struct Dinic {  //O(VVE), with minimum cut
     bool side[MAXN];
     void cut(int u) {
         side[u] = 1;
-        for ( int i : G[u] ) {
-            if ( !side[ edges[i].v ] && edges[i].rest ) cut(edges[i].v);
-        } 
+        for (int i : G[u]) {
+            if (!side[edges[i].v] && edges[i].rest) cut(edges[i].v);
+        }
     }
     // min cut end
 
-    int add_node(){
+    int add_node() {
         return n++;
     }
 
-    void add_edge(int u, int v, long long cap){
-        edges.push_back( {u, v, cap, cap} );
-        edges.push_back( {v, u, 0, 0LL} );
+    void add_edge(int u, int v, long long cap) {
+        edges.push_back({u, v, cap, cap});
+        edges.push_back({v, u, 0, 0LL});
         m = edges.size();
-        G[u].push_back(m-2);
-        G[v].push_back(m-1);
+        G[u].push_back(m - 2);
+        G[v].push_back(m - 1);
     }
-    
-    bool bfs(){
-        fill(d,d+n,-1);
+
+    bool bfs() {
+        fill(d, d + n, -1);
         queue<int> que;
-        que.push(s); d[s]=0;
-        while (!que.empty()){
-            int u = que.front(); que.pop();
-            for (int ei : G[u]){
+        que.push(s);
+        d[s] = 0;
+        while (!que.empty()) {
+            int u = que.front();
+            que.pop();
+            for (int ei : G[u]) {
                 Edge &e = edges[ei];
-                if (d[e.v] < 0 && e.rest > 0){
+                if (d[e.v] < 0 && e.rest > 0) {
                     d[e.v] = d[u] + 1;
                     que.push(e.v);
                 }
@@ -88,30 +90,30 @@ struct Dinic {  //O(VVE), with minimum cut
         return d[t] >= 0;
     }
 
-    long long dfs(int u, long long a){
-        if ( u == t || a == 0 ) return a;
+    long long dfs(int u, long long a) {
+        if (u == t || a == 0) return a;
         long long flow = 0, f;
-        for ( int &i=cur[u]; i < (int)G[u].size() ; i++ ) {
-            Edge &e = edges[ G[u][i] ];
-            if ( d[u] + 1 != d[e.v] ) continue;
-            f = dfs(e.v, min(a, e.rest) );
-            if ( f > 0 ) {
+        for (int &i = cur[u]; i < (int)G[u].size(); i++) {
+            Edge &e = edges[G[u][i]];
+            if (d[u] + 1 != d[e.v]) continue;
+            f = dfs(e.v, min(a, e.rest));
+            if (f > 0) {
                 e.rest -= f;
-                edges[ G[u][i]^1 ].rest += f;
+                edges[G[u][i] ^ 1].rest += f;
                 flow += f;
                 a -= f;
-                if ( a == 0 )break;
+                if (a == 0) break;
             }
         }
         return flow;
     }
 
-    long long maxflow(int _s, int _t){
+    long long maxflow(int _s, int _t) {
         s = _s, t = _t;
         long long flow = 0, mf;
-        while ( bfs() ){
-            fill(cur,cur+n,0);
-            while ( (mf = dfs(s, INF)) ) flow += mf;
+        while (bfs()) {
+            fill(cur, cur + n, 0);
+            while ((mf = dfs(s, INF))) flow += mf;
         }
         return flow;
     }
